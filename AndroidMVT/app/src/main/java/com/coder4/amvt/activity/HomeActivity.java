03bafdb43aa9a28ff9316b0e726b0e95edc1770a
lihy <liheyuan.bj@gmail.com>
2017-05-11 20:17:08 +0800

    MOD: Add humbuger icon to home

--- AndroidMVT/app/src/main/java/com/coder4/amvt/activity/HomeActivity.java
+++ AndroidMVT/app/src/main/java/com/coder4/amvt/activity/HomeActivity.java
@@ -1,9 +1,19 @@
 package com.coder4.amvt.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.coder4.amvt.R;
@@ -17,6 +27,10 @@ public class HomeActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private TabLayout tabLayout;
     private ListView slidingMenuListView;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+
+    private ActionBarDrawerToggle toggle;
 
     private SlidingMenuEntry[] MENU_ENTRIES = new SlidingMenuEntry[] {
             new SlidingMenuEntry("Menu1", R.drawable.ic_home),
@@ -39,6 +53,26 @@ public class HomeActivity extends AppCompatActivity {
 
         slidingMenuListView = (ListView)findViewById(R.id.slidingMenuListView);
         slidingMenuListView.setAdapter(new SlidingMenuAdapter(this, MENU_ENTRIES));
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.app_name);
+        toggle.setDrawerIndicatorEnabled(true);
+        toggle.syncState();
+        drawerLayout.addDrawerListener(toggle);
+
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
